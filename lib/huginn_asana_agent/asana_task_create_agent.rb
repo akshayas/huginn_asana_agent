05f163fbc40a3a4affb527ca966890213c3acbee
Akshaya Srivatsa <akshay.s86@gmail.com>
2019-07-22 11:24:28 -0700

    Initial set of tests for query agent (#8)

--- lib/huginn_asana_agent/asana_task_create_agent.rb
+++ lib/huginn_asana_agent/asana_task_create_agent.rb
@@ -3,27 +3,20 @@ require_relative "huginn_asana"
 
 module Agents
   class AsanaTaskQueryAgent < Agent
-    include FormConfigurable
     using HuginnAsana
 
-    SUPPORTED_FIELDS=[:id, :name, :assignee, :assignee_status, :created_at, :completed, :custom_fields, :due_at, :due_on, :resource_type, :resource_subtype]
-
-    cannot_receive_events!
+    cannot_create_events!
 
     gem_dependency_check { defined?(Asana::Client) }
 
     description do
       <<-MD
-        The Asana Task Query Agent allows to query all tasks in a given project or section or tag.
+        The Asana Task Create Agent allows to create Asana tasks given some metadata
 
         To authenticate you need to set `access_token`. You can find it within Asana
         webapp from "My Profile Settings" > Apps > Manage developer apps > Personal access token.
         Note that personal access tokens will only be shown once after which you wont be able to see it again.
         Make sure you are ready to save the access token in Huginn or in your password vault (1Password)
-
-        List of supported fields
-
-        #{SUPPORTED_FIELDS.map { |f| f.to_s }}
       MD
     end
 
@@ -33,15 +26,13 @@ module Agents
       {
         "access_token" => "",
         "id" => "",
-        "field" => "",
-        "type" => "project"
+        "type" => "",
+        "name" => "",
+        "notes" => "",
+        "assignee" => "",
+        "due_on" => ""
       }
     end
-
-    form_configurable :access_token
-    form_configurable :id
-    form_configurable :fields
-    form_configurable :type, type: :array, values: ["project", "section", "tag"]
 
     def working?
       !recent_error_logs?
@@ -49,18 +40,6 @@ module Agents
 
     def validate_options
       errors.add(:base, "you need to specify your Asana access token") unless options["access_token"].present?
-      errors.add(:base, "you need to specify a valid id") unless options["id"].present?
-      errors.add(:base, "you need to all the fields you want to receive. id and gid are included by default") unless options["fields"].present?
-      if options["fields"].present?
-        fields = options["fields"].split(",").map(&:strip)
-        errors.add(:base, "one or more of your fields is invalid. Check your input") unless verify_fields(fields)
-      end
-    end
-
-    def verify_fields(fields)
-      fields.reduce(true) do |result, field|
-        result && SUPPORTED_FIELDS.include?(field.to_sym)
-      end
     end
 
     def generate_events(result, fields)
@@ -74,13 +53,17 @@ module Agents
         c.authentication :oauth2, bearer_token: interpolated["access_token"]
       end
 
-      fields = interpolated["fields"].split(",").map(&:strip)
+      data = {
+        "name" => interpolated["name"]
+        "assignee" => interpolated["assignee"]
+      }
 
       case options["type"]
       when "project"
+
         result = asanaClient.tasks.find_by_project(projectId: interpolated["id"], options: { fields: fields })
         generate_events(result, fields)
-      when "section"
+      when "workspace"
         result = asanaClient.tasks.find_by_section(section: interpolated["id"], options: { fields: fields })
         generate_events(result, fields)
       when "tag"
--- spec/asana_task_query_agent_spec.rb
+++ spec/asana_task_query_agent_spec.rb
@@ -4,8 +4,64 @@ require "rails_helper"
 require "huginn_asana_agent/asana_task_query_agent"
 
 describe Agents::AsanaTaskQueryAgent do
-  it "should return a blank instance" do
-    k = false
-    expect(k).to be false
+  opts = {
+    access_token: "some-token",
+    id: "12345",
+    fields: "name, due_on",
+    type: "project"
+  }
+
+  let(:agent) {
+    _agent = Agents::AsanaTaskQueryAgent.new(name: "Search Asana Tasks", options: opts)
+    _agent.user = users(:bob)
+    _agent.save!
+    _agent
+  }
+
+  it "cannot receive events" do
+    expect(agent.cannot_receive_events?).to eq true
+  end
+
+  describe "#working?" do
+    it "would not be working if there are recent error" do
+      stub(agent).recent_error_logs? { true }
+			expect(agent).not_to be_working
+    end
+    
+    it "would be working if there are no recent error" do
+      stub(agent).recent_error_logs? { false }
+			expect(agent).to be_working
+		end
+	end
+
+  describe "#verify_fields" do
+    it "returns false if there exists an unsupported field" do
+      expect(agent.verify_fields(["name", "abc"])).to be false
+    end
+
+    it "returns true only if all fields are supported" do
+      expect(agent.verify_fields(["name", "due_on", "resource_type"])).to be true
+    end
+  end
+
+  context "#validate_options" do
+    it "is valid with given options" do
+      expect(agent).to be_valid
+    end
+
+    it "is invalid if access_token is missing" do
+      agent.options[:access_token] = ""
+      expect(agent).not_to be_valid
+    end
+
+    it "is invalid if id is missing" do
+      agent.options[:id] = ""
+      expect(agent).not_to be_valid
+    end
+
+    it "is invalid if fields are invalid" do
+      agent.options[:fields] = "name, due_on, invalid_field"
+      expect(agent).not_to be_valid
+    end
   end
 end
